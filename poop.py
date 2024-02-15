948a4eb3af24badcd0a4bb0d75f1bf34f27155e7
Faizan Shaikh <faizanroy000777@gmail.com>
2024-02-15 14:35:49 +0530

    CLASS VARIABLES 🚗

--- poop.py
+++ poop.py
@@ -1,4 +1,4 @@
-# from car import Car
+from car import Car
 
 # Object Oriented Programming (OOP) 🐍
 # car1 = Car("Chevy", "Corvette", 2021, "blue")
@@ -11,3 +11,13 @@
 
 # car1.drive()
 # car2.stop()
+
+# CLASS VARIABLES 🚗
+# car1 = Car("Chevy", "Corvette", 2021, "blue")
+# car2 = Car("Ford", "Mustang", 2022, "black")
+
+# Car.wheels = 2 # global change
+
+# car1.wheels = 2
+# print(car1.wheels)
+# print(car2.wheels)
