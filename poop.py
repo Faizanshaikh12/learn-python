501dce89c49d0efa75178db3f08afba373d2aa67
Faizan Shaikh <faizanroy000777@gmail.com>
2024-02-23 15:48:47 +0530

    METHOD CHAINING ⛓️

--- poop.py
+++ poop.py
@@ -110,3 +110,24 @@ from car import Car
 
 # prey = Prey2()
 # prey.flee()
+
+# METHOD CHAINING ⛓️ = calling multiple methods sequentially each call performs an action on the same object and returns self.
+# class Car:
+#     def turn_on(self):
+#         print("You start the engine")
+#         return self
+    
+#     def drive(self):
+#         print("You drive the car")
+#         return self
+    
+#     def brake(self):
+#         print("You step on brakes")
+#         return self
+    
+#     def turn_off(self):
+#         print("You turn off the engine")
+#         return self
+    
+# car = Car()
+# car.turn_on().drive().brake().turn_off()
