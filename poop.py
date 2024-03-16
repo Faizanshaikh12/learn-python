d05f615e3bd9dd6c98bc3a669ede8064ee83bbc0
Faizan Shaikh <faizanroy000777@gmail.com>
2024-03-16 13:23:12 +0530

    if _name_ == '__main__' ❓

--- poop.py
+++ poop.py
@@ -281,3 +281,17 @@ from car import Car
 # users = zip(usernames, passwords)
 # for i in users:
 #     print(i)
+
+# if _name_ == '__main__' ❓
+# 1. Module can be run as a standalone program
+# 2. Module can be imported and used by other modules
+
+# Python interpreter sets "special variables", one of which is __name__
+# then Python will execute the code found within __main__
+
+# if __name__ == "__main__":
+#     pass
+
+# import car
+# print(__name__)
+# print(car.__name__)
