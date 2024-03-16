cfeda8e2b0768166fecf39137e399f376351b898
Faizan Shaikh <faizanroy000777@gmail.com>
2024-03-16 13:05:56 +0530

    WALRUS OPERATOR 🦦

--- poop.py
+++ poop.py
@@ -252,3 +252,22 @@ from car import Car
 # person = Person()
 # person.catch(duck)
 # person.catch(chicken)
+
+# WALRUS OPERATOR 🦦
+# new to python 3.8
+# assignment expression aka walrus operator
+# assigns values to variable as part of a larger expression
+
+# happy = True
+# print(happy := True)
+
+# foods = list()
+# while True:
+#     food  = input("What food do you like?: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+foods = list()
+while food := input("What food do you like?: ") != "quit":
+    foods.append(food)
