8bb0b447ad0d2f5e51d1d073a3078603cded8aea
Faizan Shaikh <faizanroy000777@gmail.com>
2024-05-02 23:24:35 +0530

    labels 🏷️

--- gui.py
+++ gui.py
@@ -4,14 +4,34 @@ from tkinter import *
 # widgets =  GUI elements: buttons, textboxes, labels, images
 # windows = Serves as a container to hold or contain these widgets
 
-window = Tk(); #instantiate an instance of window
-window.geometry("420x420")
+# window = Tk(); #instantiate an instance of window
+# window.geometry("420x420")
 
-window.title("My First Tkinter Program")
+# window.title("My First Tkinter Program")
 
-icon = PhotoImage(file='python-logo.png')
-window.iconphoto(True, icon)
+# icon = PhotoImage(file='python-logo.png')
+# window.iconphoto(True, icon)
 
-window.config(background="#5C5CFF")
+# window.config(background="#5C5CFF")
 
-window.mainloop() # place window on computer screen. listen for evetns
+# window.mainloop() # place window on computer screen. listen for evetns
+
+# labels 🏷️
+# window = Tk()
+
+# # Load the image
+# image = PhotoImage(file="python-logo.png")
+
+# # Resize the image to desired dimensions
+# width, height = 100, 100  # Set your desired width and height
+# image = image.subsample(image.width() // width, image.height() // height)
+
+# # Create a label with both text and image
+# label = Label(window, text="Hello Python", image=image, compound="bottom")
+# label.pack()
+
+# # Center the image within the label
+# label.image = image  # Keep a reference to the image to prevent garbage collection
+# label.config(anchor=CENTER)
+
+# window.mainloop()
